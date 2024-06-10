7622484029e99d8c217a6f424c76bd68a631326d
Vitor Pires <88112014+vikpires@users.noreply.github.com>
2024-06-09 23:07:37 -0300

    organize folders and install vue

---
+++ calculator-ts/src/calculator/menu.ts
@@ -0,0 +1,33 @@
+const readlineSync = require('readline-sync');
+import { Calculator, AddOperation, SubtractOperation, MultiplyOperation, DivideOperation } from "../calculator/calculator";
+
+// Display menu and select operations
+export class Menu {
+    public static display(): void {
+        console.log(`
+            ----------CALCULADORA----------
+
+            1 - Adição (+)
+            2 - Subtração (-)
+            3 - Multiplicação (*)
+            4 - Divisão (/)
+            5 - Sair        
+
+            -------------------------------
+            `);
+    }
+    public static getCalculateOption(choice: string): Calculator | null {
+        switch (choice) {
+            case '1': return new AddOperation();
+            case '2': return new SubtractOperation();
+            case '3': return new MultiplyOperation();
+            case '4': return new DivideOperation();
+            case '5':
+                console.log('\nObrigado por usar a calculadora. Até a próxima!\n');
+                process.exit(0);
+            default:
+                console.log('Erro: Operação não reconhecida. Tente novamente.\n');
+                return null;
+        }     
+    }
+}
